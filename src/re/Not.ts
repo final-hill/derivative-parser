58983b5b300149c3e3c2204292fa8213ecb7b04b
Michael Haufe <tno@thenewobjective.com>
2020-07-10 12:33:18 -0500

    - partial NOT pattern implementation

---
+++ src/re/Not.ts
@@ -0,0 +1,19 @@
+/*!
+ * @license
+ * Copyright (C) 2020 Michael L Haufe
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * @see <https://spdx.org/licenses/AGPL-3.0-only.html>
+ */
+
+import RegularLanguage from './RegularLanguage';
+
+/**
+ *
+ */
+class Not extends RegularLanguage {
+    constructor(readonly language: RegularLanguage) {
+        super(1 + language.height);
+    }
+}
+
+export default Not;
