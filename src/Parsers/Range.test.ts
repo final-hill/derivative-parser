62f918522fddb278119c9b390e0fdc1019daf3cf
Michael Haufe <tno@thenewobjective.com>
2020-10-31 18:44:40 -0500

    Updated Range unit tests

--- src/Parsers/Range.test.ts
+++ src/Parsers/Range.test.ts
@@ -5,16 +5,29 @@
  * @see <https://spdx.org/licenses/AGPL-3.0-only.html>
  */
 
+import {Parser} from "./";
+
 describe('Range', () => {
+    const p = new Parser(),
+          digit = p.range('1','9');
+
     test('Range.deriv(c)', () => {
-        // TODO
+        expect(p.range('1','1').deriv('1')).toEqual(p.empty());
+        expect(p.range('1','1').deriv('x')).toEqual(p.nil());
+        expect(digit.deriv('x')).toEqual(p.nil());
+        expect(digit.deriv('8')).toEqual(p.empty());
+    });
+
+    test('Range.isRange', () => {
+        expect(digit.isRange()).toBe(true);
     });
 
     test('Range.matches(c)', () => {
-        // TODO
+        expect(digit.matches('2')).toBe(true);
+        expect(digit.matches('x')).toBe(false);
     });
 
     test('Range.toString()', () => {
-        // TODO
+        expect(digit.toString()).toBe('[1-9]');
     });
 });
