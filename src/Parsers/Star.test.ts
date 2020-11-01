df234092fa1d2de85798225fbdec9c1b09599a4b
Michael Haufe <tno@thenewobjective.com>
2020-11-01 14:22:53 -0600

    Updated Star unit test

--- src/Parsers/Star.test.ts
+++ src/Parsers/Star.test.ts
@@ -10,6 +10,10 @@ import {Parser} from "./";
 describe('Star', () => {
     const p = new Parser();
 
+    test('Star.containsEmpty', () => {
+        expect(p.char('a').star().containsEmpty()).toBe(true);
+    });
+
     test('Star.deriv(c)', () => {
         const p1 = p.char('a');
         expect(
@@ -19,12 +23,38 @@ describe('Star', () => {
         );
     });
 
+    test('Star.equals', () => {
+        expect(p.char('a').star().equals(p.char('a').star())).toBe(true);
+        expect(p.char('a').star().equals(p.char('s'))).toBe(false);
+    });
+
+    test('Star.height', () => {
+        expect(p.char('a').star().height).toBe(1);
+        expect(p.alt('a',p.alt('b','c')).star().height).toBe(3);
+    });
+
+    test('Star.isStar', () => {
+        expect(p.char('a').star().isStar()).toBe(true);
+        expect(p.char('a').isStar()).toBe(false);
+    });
+
     test('Star.matches(c)', () => {
         const p1 = p.char('a').star();
         expect(p1.matches('')).toBe(true);
         expect(p1.matches('a')).toBe(true);
         expect(p1.matches('aaa')).toBe(true);
         expect(p1.matches('b')).toBe(false);
+    });
+
+    test('Star.nilOrEmpty', () => {
+        expect(p.char('a').star().nilOrEmpty()).toEqual(p.empty());
+    });
+
+    test('Star.simplify', () => {
+        expect(p.nil().star().simplify()).toEqual(p.empty());
+        expect(p.empty().star().simplify()).toEqual(p.empty());
+        expect(p.char('a').star().star().simplify()).toEqual(p.char('a').star());
+        expect(p.char('a').star().simplify()).toEqual(p.char('a').star());
     });
 
     test('Star.toString()', () => {
