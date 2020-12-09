95958f9bdc4dbbb98a21a4a7ca4e059925b9ca40
Michael Haufe <tno@thenewobjective.com>
2020-12-09 09:14:55 -0600

    Grammar extends Parser

--- src/Grammar.ts
+++ src/Grammar.ts
@@ -6,13 +6,15 @@
  */
 
 import {Parser, Thunk} from './Parsers';
+import Contracts from '@final-hill/decorator-contracts';
 
-const p = new Parser();
+const contracts = new Contracts(true),
+    {override} = contracts;
 
 /**
  * The Grammar class represents
  */
-class Grammar {
+class Grammar extends Parser {
     protected _handler: ProxyHandler<Record<PropertyKey, unknown>> = {
         // TODO: Change request to TypeScript repo to
         // align Proxy.get and Reflect.get documentation
@@ -32,6 +34,8 @@ class Grammar {
     };
 
     constructor() {
+        super();
+
         return new Proxy(this, this._handler as any);
     }
 
@@ -43,10 +47,11 @@ class Grammar {
      * @returns {boolean} - The result of the test
      * @throws - If text is not a string
      */
+    @override
     matches(text: string): boolean {
         const ruleNames = Object.getOwnPropertyNames(Object.getPrototypeOf(this))
                         .filter(name => name != 'constructor' && typeof (this as any)[name] == 'function'),
-            entryPoint: Parser = ruleNames.length == 0 ? p.nil() : (this as any)[ruleNames[0]].apply(this);
+            entryPoint: Parser = ruleNames.length == 0 ? this.nil() : (this as any)[ruleNames[0]].apply(this);
 
         return entryPoint.matches(text);
     }
